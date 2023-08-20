4aff8c3625e29238da7261392377e675e34dba24
Santiago Ariel Villanueva <santiagovillanuevaariel@gmail.com>
2023-08-20 14:59:47 -0300

    ejercicio pre parcial

--- 1-introduccion a la programacion/tp-2/ejercicio-29-if/main.c
+++ 1-introduccion a la programacion/tp-2/ejercicio-29-if/main.c
@@ -19,25 +19,26 @@ int main(int argc, char const *argv[])
   printf("3) Calcular el area de un triangulo equilatero\n");
   scanf("%d", &opcion);
 
-  switch (opcion)
+  if (opcion == 1)
   {
-  case 1:
     printf("Ingrese el lado del cuadrado: ");
     scanf("%f", &lado);
 
     area = lado * lado;
 
     printf("El area del cuadrado es de: %.2f\n", area);
-    break;
-  case 2:
+  }
+  else if (opcion == 2)
+  {
     printf("Ingrese el radio del circulo: ");
     scanf("%f", &radio);
 
     area = 3.14 * radio * radio;
 
     printf("El area del circulo es de: %.2f\n", area);
-    break;
-  case 3:
+  }
+  else if (opcion == 3)
+  {
     printf("Ingrese la base del triangulo: ");
     scanf("%f", &base);
 
@@ -47,10 +48,10 @@ int main(int argc, char const *argv[])
     area = (base * altura) / 2;
 
     printf("El area del triangulo es de: %.2f\n", area);
-    break;
-  default:
+  }
+  else
+  {
     printf("La opcion ingresada no es valida\n");
-    break;
   }
 
   return 0;
